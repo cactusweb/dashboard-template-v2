f9cd097a244b2499028398c80d20978afad72555
gaspromov <gaspromov@mail.ru>
2023-03-22 21:52:50 +0300

    Ready for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.cactusweb.io/api/v2/user',
 
-  ownerName: 'CactusSoft',
-  site: 'http://localhost:4200',
-  landing: '',
+  ownerName: 'Quantum IO',
+  site: 'https://dash.qntmru.io/',
+  landing: 'https://qntmru.io/',
   
   logo: '/assets/logo.svg',
   paymentInFrame: false
