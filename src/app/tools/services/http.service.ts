31e7906c1ea6aacc40a668844138f0f21aaad807
gaspromov <gaspromov@mail.ru>
2023-09-14 00:38:10 +0300

    [fix] Добавил BODY для http

--- src/app/tools/services/http.service.ts
+++ src/app/tools/services/http.service.ts
@@ -5,43 +5,57 @@ import { environment } from 'src/environments/environment';
 import { Req } from '../interfaces/req-map';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
   private url = environment.apiUrl;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
-  
-  request( reqParams: Req, body: any = '', urlParam: string = '', urlQuery: string = '' ): Observable<any>{
+  request(
+    reqParams: Req,
+    body: any = '',
+    urlParam: string = '',
+    urlQuery: string = ''
+  ): Observable<any> {
     let reqUrl = this.url + reqParams.url;
 
-    reqUrl = reqUrl.replace( ':param', urlParam ) + urlQuery;
+    reqUrl = reqUrl.replace(':param', urlParam) + urlQuery;
 
-    switch ( reqParams.method ){
-      case "POST": return this.postHttp( reqUrl, body );
-      case "GET": return this.getHttp( reqUrl );
-      case "DELETE": return this.deleteHttp( reqUrl );
-      case "PUT": return this.putHttp( reqUrl, body );
-      default: return this.getHttp( reqUrl )
+    switch (reqParams.method) {
+      case 'POST':
+        return this.postHttp(reqUrl, body);
+      case 'GET':
+        return this.getHttp(reqUrl);
+      case 'DELETE':
+        return this.deleteHttp(reqUrl, body);
+      case 'PUT':
+        return this.putHttp(reqUrl, body);
+      default:
+        return this.getHttp(reqUrl);
     }
   }
 
-  private postHttp( url: string, data: Record< string, any > ){
-    return this.http.post( url, data );
+  private postHttp(url: string, data: Record<string, any>) {
+    return this.http.post(url, data);
   }
 
-  private getHttp( url: string ){
-    return this.http.get( url );
+  private getHttp(url: string) {
+    return this.http.get(url);
   }
 
-  private putHttp( url: string, data: Record< string, any > ){
-    return this.http.put( url, data );
+  private putHttp(url: string, data: Record<string, any>) {
+    return this.http.put(url, data);
   }
 
-  private deleteHttp( url: string ){
-    return this.http.delete( url );
+  private deleteHttp(url: string, body: any) {
+    return this.http.delete(
+      url,
+      body
+        ? {
+            body,
+          }
+        : undefined
+    );
   }
 }
