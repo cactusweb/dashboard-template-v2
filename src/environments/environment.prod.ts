d3d1fb5b8fa0d3b4ab8681a61fc0ea0b6e841b10
gaspromov <gaspromov@mail.ru>
2024-02-02 00:03:51 +0300

    [minor] Domain changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.cactusweb.io/api/v2/user',
+  apiUrl: 'https://api.spmembership.com/api/v2/user',
 
   ownerName: 'SPirates',
-  site: 'https://dash.spirates.io',
-  landing: 'https://spirates.io',
-  
+  site: 'https://dash.spmembership.com',
+  landing: 'https://spmembership.com',
+
   logo: '/assets/logo.svg',
-  paymentInFrame: false
+  paymentInFrame: false,
 };
